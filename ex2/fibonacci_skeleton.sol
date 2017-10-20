afbb95bf48f41b4d50278c751400b456949e89b1
BayAreaBabe <jenniferhan111@gmail.com>
2017-10-20 09:54:09 -0700

    Update fibonacci_skeleton.sol

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -4,8 +4,16 @@ pragma solidity ^0.4.16;
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
 		/* Carry out calculations to find the nth Fibonacci number */
+		require(position>=0)
+		if (position == 0) {
+			return result = 0;
+		} else if (position == 1) {
+			return result = 1
+		} else calculate(position-1 + position-2);
+			
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
-
+	function() {
+	}
 }

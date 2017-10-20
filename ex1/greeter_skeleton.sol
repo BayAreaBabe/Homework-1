b25bf4057a4bbdca7198dcccee8ea6eb6d6bdae1
BayAreaBabe <jenniferhan111@gmail.com>
2017-10-20 09:16:19 -0700

    Update greeter_skeleton.sol

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -4,15 +4,19 @@ pragma solidity ^0.4.16;
 contract Greeter {
 	/* Add one variable to hold our greeting */
 	string greeting;
+	
 
 	function Greeter(string _greeting) public {
 		/* Write one line of code for the contract to set our greeting */
+		greeting = "Hello World!";
 	}
 
 	function greet() constant returns (string)  {
 		/* Write one line of code to allow the contract to return our greeting */
+		return greeting;
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
-	
+	function {
+	}
 }
